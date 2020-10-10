47f6ee8e10ec96168eca06fddf5876d7fe14523b
YETIApp <jannislp.ytub@gmail.com>
2020-10-10 15:01:28 +0200

    windows build

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,13 +3,15 @@ import { HTTP } from '@ionic-native/http/ngx';
 import { HttpClient } from "@angular/common/http"
 
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   constructor(private http: HttpClient) { }
-
+  
 
   getHGWData(){
     return this.http.get("https://hgw-hofweier.de/api/hgw1.json");
